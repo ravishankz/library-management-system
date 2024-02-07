71a864df187d284b8d0dbb5bade599777c04b197
ravishankz <ravishankz@users.noreply.github.com>
2024-02-07 21:35:50 +0530

    bootstrap implemented for user login page.

--- pages/login.php
+++ pages/login.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Login</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -12,23 +14,25 @@
     <div class="container">
         <h2>User Login</h2>
         <?php
-        
         if (isset($_SESSION['error'])) {
-            echo '<p class="error">' . $_SESSION['error'] . '</p>';
+            echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error'] . '</div>';
             unset($_SESSION['error']);
         }
         ?>
         <form action="../scripts/login_script.php" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            <br>
-            <button type="submit">Login</button>
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
         </form>
     </div>
 
     <?php include('../includes/footer.php'); ?>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
