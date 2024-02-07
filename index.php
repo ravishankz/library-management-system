4e49abc1d0f890ebb93d163fa715e103e4ccd7a5
ravishankz <ravishankz@users.noreply.github.com>
2024-02-07 11:35:40 +0530

    Initial commit - boilerplate

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container">
+    <h1>Welcome to the Library Management System</h1>
+    <!-- Add content here -->
+</div>
+
+<?php include('includes/footer.php'); ?>
