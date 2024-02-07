0a13b8482611ba028df76b0be72da59a333aa9aa
ravishankz <ravishankz@users.noreply.github.com>
2024-02-07 21:50:43 +0530

    bootstrap implemented for update_user_form page.

--- scripts/update_user_form.php
+++ scripts/update_user_form.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 include('../config.php');
@@ -15,13 +14,13 @@ $userId = htmlspecialchars(stripslashes(trim($_GET['user_id'])));
 $query = "SELECT * FROM user WHERE user_id='$userId'";
 $result = $conn->query($query);
 
-if ($result->num_rows == 0) {
+if ($result && $result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+} else {
     $_SESSION['error'] = "User not found.";
     header("Location: ../view_users.php");
     exit();
 }
-
-$user = $result->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +29,8 @@ $user = $result->fetch_assoc();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update User Details</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
@@ -39,28 +40,33 @@ $user = $result->fetch_assoc();
         <h2>Update User Details</h2>
         <?php
         if (isset($_SESSION['error'])) {
-            echo '<p class="error">' . $_SESSION['error'] . '</p>';
+            echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error'] . '</div>';
             unset($_SESSION['error']);
         }
         ?>
         <form action="update_user.php" method="POST">
             <input type="hidden" name="userId" value="<?php echo $user['user_id']; ?>">
-            <label for="firstName">First Name:</label>
-            <input type="text" id="firstName" name="firstName" value="<?php echo $user['first_name']; ?>" required>
-            <br>
-            <label for="lastName">Last Name:</label>
-            <input type="text" id="lastName" name="lastName" value="<?php echo $user['last_name']; ?>" required>
-            <br>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" value="<?php echo $user['username']; ?>" required>
-            <br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>" required>
-            <br>
-            <button type="submit">Update</button>
+            <div class="form-group">
+                <label for="firstName">First Name:</label>
+                <input type="text" id="firstName" name="firstName" class="form-control" value="<?php echo $user['first_name']; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="lastName">Last Name:</label>
+                <input type="text" id="lastName" name="lastName" class="form-control" value="<?php echo $user['last_name']; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" class="form-control" value="<?php echo $user['username']; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" class="form-control" value="<?php echo $user['email']; ?>" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Update</button>
         </form>
     </div>
 
     <?php include('../includes/footer.php'); ?>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
