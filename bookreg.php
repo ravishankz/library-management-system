c6e44c73bfacca849c57416c49f9149678e147e3
M. Chamod Sasitha Gunathilaka <chamodsasitha75@gmail.com>
2024-02-14 22:10:57 +0530

    Chaged few things and completed code

--- bookreg.php
+++ bookreg.php
@@ -8,13 +8,20 @@ require_once('configuration.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="bookreg.css">
     <title>Library Management System</title>
 </head>
 <body>
-    <h1>Library Management System</h1>
 
-    <h2>Register a Book</h2>
+<nav class="navbar">
+<h1>LIBRARY MANAGEMENT SYSTEM</h1>
+<br><br><br>
+</nav>
+<nav class="navbar2"><br><br>
+<h2>Register a Book</h2>
+</nav>
+    
+   
     <form action="library.php" method="post">
         <label for="bookID">Book ID:</label>
         <input type="text" id="bookID" name="bookID" value='<?php echo $id ?>' required pattern="B\d{3}" title="Please enter Book ID in the format B001." ><br><br>
@@ -35,5 +42,43 @@ require_once('configuration.php');
         <?php endif; ?>
     </form>
 
-    </body>
+    <h2>Display All Books</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Book ID</th>
+                <th>Book Name</th>
+                <th>Book Category</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+                $sql = "SELECT * FROM book";
+                $result = $conn->query($sql);
+            
+                if ($result->num_rows > 0){
+
+                    while($row = $result->fetch_assoc()){
+                      ?>  <tr>
+                            <td> <?php echo $row['book_id'] ?> </td>
+                            <td> <?php echo $row['book_name'] ?> </td>
+                            <td> <?php echo $row['category_id'] ?> </td>
+
+                            <td> <a href="book.php?edit=<?php echo $row['book_id'] ?>"><button class="b1">Edit</button></a>
+
+                            <a href="book.php?delete=<?php echo $row['book_id'] ?>"><button class="b2">Delete</button></a>
+                        </tr>
+                    <?php }
+                }else{
+                    echo "result = 0";
+                }
+
+                $conn->close();
+                ?>
+        </tbody>
+    </table>
+</body>
 </html>
