3e893b08904bebed5c767711663f7282ff6f328d
Kanchana9889 <148602337+Kanchana9889@users.noreply.github.com>
2024-02-14 13:41:07 +0530

    Update members php errors

--- members.php
+++ members.php
@@ -170,18 +170,6 @@ if (isset($_POST['update'])) {
             <tbody>
 
             <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "library_system";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 $result = $conn->query("SELECT * FROM member");
 
