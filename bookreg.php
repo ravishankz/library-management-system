ea1dc7edd0535cb9d127b1c9bddc569fea12129c
M. Chamod Sasitha Gunathilaka <chamodsasitha75@gmail.com>
2024-02-14 21:33:44 +0530

    Added link to external css file

--- bookreg.php
+++ bookreg.php
@@ -8,6 +8,7 @@ require_once('configuration.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <title>Library Management System</title>
 </head>
 <body>
